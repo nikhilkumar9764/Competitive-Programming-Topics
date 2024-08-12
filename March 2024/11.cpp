c6f6a077b279d79be885d4fa17f2eeb5c1ffde0e
NIKHIL KUMAR <nikhil.kumar9764@gmail.com>
2024-08-12 15:39:53 +0530

    Aug_commit_prev

---
+++ March 2024/11.cpp
@@ -0,0 +1,22 @@
+/* Microsoft OA questions : 
+Given a set of edges to form a graph network, now assign weights to the nodes such that total sum of weights of edges is maximum most . 
+
+Given if the vertices set is : [[2,4] , [2,1] , [2,3] ,[3,1] ,[4,5]] , so degree of node 2 is maximum followed by degree of node 3 
+and then degree of node 1 .
+*/
+
+/* OA Questions : 
+Q1 . Given a number X , find the next bigger number according to the digits 
+
+Q2. Have the function StringReduction(str) take the str parameter being passed and return the smallest number you can get 
+through the following reduction method. The method is: Only the letters a, b, and c will be given in str and you must take two
+ different adjacent characters and replace it with the third. For example "ac" can be replaced with "b" but "aa" cannot be replaced 
+ with anything. 
+This method is done repeatedly until the string cannot be further reduced, and the length of the resulting string is to be outputted.
+
+*/
+
+/* Trie based questions  : 
+
+Q1. Find the most occurring word in a given set of strings 
+Q2. Find the longest common prefix in a set of strings */
